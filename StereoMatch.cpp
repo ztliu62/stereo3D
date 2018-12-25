d6bad1307a6d1a1ddd1b1807efe39e4e73510879
jerry <liuzhongtao62@gmail.com>
2018-12-24 22:57:05 -0500

    OpenCV implementation

--- StereoMatch.cpp
+++ StereoMatch.cpp
@@ -12,13 +12,70 @@ using namespace std;
 
 StereoOpenCV::StereoOpenCV() {
     this->paramCount = 3;
-    this->params[0] = 11;
-    this->params[1] = 6*16;
-    this->params[2] = 0;
+    this->params[0] = 11; // SAD Windows Size
+    this->params[1] = 5*16; // Number of Disparity
+    this->params[2] = 0; // min Disparity
 }
 
 string StereoOpenCV::getKindName() {
     return "SGBM_OpenCV";
 }
 
+int StereoOpenCV::getParamCount() {
+    return this->paramCount;
+}
+
+void StereoOpenCV::setParamValue(int index, int value) {
+
+    if (index == 0){
+        if (value%2 == 0){
+            value += 1;
+        }
+    } else if (index == 1){
+        if (value%16 != 0){
+            value = (value/16)*16;
+        }
+    } else if (index == 2){
+
+    } else {
+        return;
+    }
+    this->params[index] = value;
+}
+
+void StereoOpenCV::stereomatch(Mat &left, Mat &right, Mat &disp){
+    Mat leftgray, rightgray;
+    leftgray.create(left.size(), CV_8UC1);
+    rightgray.create(right.size(), CV_8UC1);
+    cvtColor(left, leftgray, CV_BGR2GRAY);
+    cvtColor(right, rightgray, CV_BGR2GRAY);
+
+    int SADWinSize = this->params[0];
+    int numofDisparity = this->params[1];
+    int minDisparity = this->params[2];
+    int cn = 1;
+
+    Ptr<StereoSGBM> sgbm = StereoSGBM::create();
+    sgbm->setPreFilterCap(63);
+    sgbm->setBlockSize(SADWinSize);
+    sgbm->setP1(8*cn*SADWinSize*SADWinSize);
+    sgbm->setP2(32*cn*SADWinSize*SADWinSize);
+    sgbm->setMinDisparity(minDisparity);
+    sgbm->setNumDisparities(numofDisparity);
+    sgbm->setUniquenessRatio(10);
+    sgbm->setSpeckleWindowSize(100);
+    sgbm->setSpeckleRange(32);
+    sgbm->setDisp12MaxDiff(1);
+    sgbm->setMode(StereoSGBM::MODE_SGBM);
+
+    Mat disp1;
+    sgbm->compute(leftgray, rightgray, disp1);
+    disp1.convertTo(disp, CV_8U, 255/(numofDisparity*16.));
+
+    cv::Mat disp_color;
+    cv::applyColorMap(disp, disp_color, cv::COLORMAP_JET);
+
+    cv::imshow("Disparity", disp_color);
+    cv::waitKey(0);
+}
 
--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 
+#include "StereoMatch.h"
+
 using namespace std;
 
 int main() {
@@ -11,43 +13,14 @@ int main() {
     cv::Mat left = cv::imread("../images/PianoL/im0.png", 1);
     cv::Mat right = cv::imread("../images/PianoL/im1.png", 1);
     cout << left.rows << " " << left.cols << endl;
+
+    Mat disp;
     //cv::imshow("image", left);
     //cv::waitKey(0);
-
-    cv::Mat leftgray, rightgray;
-    leftgray.create(left.size(), CV_8UC1);
-    rightgray.create(right.size(), CV_8UC1);
-
-    cv::cvtColor(left, leftgray, CV_BGR2GRAY);
-    cv::cvtColor(right, rightgray, CV_BGR2GRAY);
-
-    int SADWinSize = 5;
-    int numofDisparity = 5*16;
-    int minDisparity = 0;
-    int cn = 1;
-
-    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create();
-    sgbm->setPreFilterCap(63);
-    sgbm->setBlockSize(SADWinSize);
-    sgbm->setP1(8*cn*SADWinSize*SADWinSize);
-    sgbm->setP2(32*cn*SADWinSize*SADWinSize);
-    sgbm->setMinDisparity(minDisparity);
-    sgbm->setNumDisparities(numofDisparity);
-    sgbm->setUniquenessRatio(10);
-    sgbm->setSpeckleWindowSize(100);
-    sgbm->setSpeckleRange(32);
-    sgbm->setDisp12MaxDiff(1);
-    sgbm->setMode(cv::StereoSGBM::MODE_SGBM);
-
-    cv::Mat disp, disp8;
-    sgbm->compute(leftgray, rightgray, disp);
-    disp.convertTo(disp8, CV_8U, 255/(numofDisparity*16.));
-
-    cv::Mat disp_color;
-    cv::applyColorMap(disp8, disp_color, cv::COLORMAP_JET);
-
-    cv::imshow("Disparity", disp_color);
-    cv::waitKey(0);
+    StereoOpenCV sgbm;
+    cout << sgbm.getKindName() << endl;
+    cout << sgbm.getParamCount() << endl;
+    sgbm.stereomatch(left, right, disp);
 
     return 0;
 }
